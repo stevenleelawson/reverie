a2f52244226e17eae02bfa678dcdde4a4a56f9e6
Steven Lawson <unluckygamblermusic@gmail.com>
2018-05-18 21:39:10 -0600

    Add migrations and seeded data, first GET test passing in mocha

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ app.use(bodyParser.json())
 
 app.set('port', process.env.PORT || 3000);
 
+app.get('/', (request, response) => {
+
+});
+
+app.get('/api/v1/robots', (request, response) => {
+  database('robots').select()
+    .then(robots => {
+      response.status(200).json(robots)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`Reverie's backend is running on port ${app.get('port')}`)
 })
+
+module.exports = app;
